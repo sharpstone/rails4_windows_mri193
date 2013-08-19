00ae2419c2d1ec775bc891003f5e0d05d7fd362e
Richard Schneeman <richard.schneeman@gmail.com>
2013-08-19 14:19:25 -0500

    'initial'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Rails4WindowsMri193::Application.config.secret_key_base = '4bac78f6b23398fe02968d557f58d892f937e3bd91e522152490876e2d5643df227c31712e17ddbb3bb191b5da2918ee829e046d395d61151d09b39369fecce9'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Rails4WindowsMri193::Application.config.session_store :cookie_store, key: '_rails4_windows_mri193_session'
